6474c6d26e315669306ef58799934898c38cfeed
ByMarqueZz <bymarquezz2@gmail.com>
2023-10-31 13:15:15 +0100

    nuevo commit2

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,10 @@ public class Main {
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-            /*asdasd*/
+            /*holasad*/
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
 
             // Press Ctrl+D to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Cmd+F8.
