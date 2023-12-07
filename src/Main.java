1f8871360183a7e74f89a332b54ae76872fc7ecd
ByMarqueZz <bymarquezz2@gmail.com>
2023-12-07 13:02:58 +0100

    Mi parte acabada

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,48 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.concurrent.ConcurrentHashMap;
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
 
-        /*esta es mi rama*/
+        System.out.println("________________________________");
+        System.out.println("Mostramos el mapa");
+        map.forEach((key, value) -> {
+            System.out.println(key + " " + value);
+        });
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-            /*holasad*/
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
+        int getValue = map.getOrDefault("A", 1);
+        System.out.println("________________________________");
+        System.out.println("Obtenemos el valor de la llave A");
+        System.out.println(getValue);
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        map.putIfAbsent("C", 3);
+        map.replace("B", 2, 20);
+        map.replace("B", 12311231, 211);
+        map.remove("C", 3);
+
+        System.out.println("________________________________");
+        System.out.println("Mostramos el mapa después de las operaciones atómicas");
+        map.forEach((key, value) -> {
+            System.out.println(key + " " + value);
+        });
+
+        int total = map.reduceValues(2, Integer::sum);
+        System.out.println("________________________________");
+        System.out.println("Total: " + total);
+
+        String result = map.search(2, (key, val) -> val > 10 ? key +" "+ val: null);
+        System.out.println("________________________________");
+        System.out.println("Resultado: " + result);
+
+        ConcurrentHashMap.KeySetView<String, Boolean> keySet = map.newKeySet();
+        keySet.add("D");
+        keySet.add("E");
+
+        System.out.println("________________________________");
+        System.out.println("Mostramos el keySet");
+        keySet.forEach((key) -> {
+            System.out.println(key);
+        });
     }
 }
